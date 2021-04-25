c32c7f9ca4bdb9f4b4d23f972133aad9030cd7ff
Brian Do <briancldo@gmail.com>
2021-04-24 21:20:04 -0400

    static piece disappears when clicked, reappears on mouse release

--- src/components/Pieces/MovablePiece.jsx
+++ src/components/Pieces/MovablePiece.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Piece from './Piece';
 import './MovablePiece.css';
 
 export default function MovablePiece(props) {
   const { containingPiece } = props;
+  const [dragging, setDragging] = useState(false);
+  const [staticPieceVisible, setStaticPieceVisible] = useState(true);
 
+  function mouseUpHandler() {
+    setDragging(false);
+  }
+
+  useEffect(() => {
+    if (dragging) {
+      console.log('dragging');
+      setStaticPieceVisible(false);
+      // window.addEventListener('mousemove' /* update floatingPiecePosition */);
+      window.addEventListener('mouseup', mouseUpHandler);
+    } else {
+      console.log('no dragging');
+      setStaticPieceVisible(true);
+      // window.removeEventListener(
+      //   'mousemove' /* update floatingPiecePosition */
+      // );
+      window.removeEventListener('mouseup', mouseUpHandler);
+    }
+
+    return () => {
+      window.removeEventListener('mouseup', mouseUpHandler);
+    };
+  }, [dragging]);
+
+  if (!containingPiece) return null;
   return (
-    <div className='movable-piece'>
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      className='movable-piece'
+      onMouseDown={() => setDragging(true)}
+      draggable={true}
+      hidden={!staticPieceVisible}
+    >
       <Piece {...{ containingPiece }} />
     </div>
   );
