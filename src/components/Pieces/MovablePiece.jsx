3afb2aaf7195f7098480db15aa3e00fc04b15961
Brian Do <briancldo@gmail.com>
2021-04-24 21:47:09 -0400

    implements floating piece

--- src/components/Pieces/MovablePiece.jsx
+++ src/components/Pieces/MovablePiece.jsx
@@ -7,41 +7,66 @@ export default function MovablePiece(props) {
   const { containingPiece } = props;
   const [dragging, setDragging] = useState(false);
   const [staticPieceVisible, setStaticPieceVisible] = useState(true);
+  const [floatingPiecePosition, setFloatingPiecePosition] = useState({});
+  const viewWidth = window.innerWidth;
+
+  function mouseDownHandler(event) {
+    setDragging(true);
+    mouseMoveHandler(event);
+  }
 
   function mouseUpHandler() {
     setDragging(false);
   }
 
+  function mouseMoveHandler(event) {
+    const { clientX, clientY } = event;
+    setFloatingPiecePosition({
+      x: clientX - 0.5 * 0.04 * viewWidth,
+      y: clientY - 0.5 * 0.04 * viewWidth,
+    });
+  }
+
   useEffect(() => {
     if (dragging) {
-      console.log('dragging');
       setStaticPieceVisible(false);
-      // window.addEventListener('mousemove' /* update floatingPiecePosition */);
+      window.addEventListener('mousemove', mouseMoveHandler);
       window.addEventListener('mouseup', mouseUpHandler);
     } else {
-      console.log('no dragging');
       setStaticPieceVisible(true);
-      // window.removeEventListener(
-      //   'mousemove' /* update floatingPiecePosition */
-      // );
+      window.removeEventListener('mousemove', mouseMoveHandler);
       window.removeEventListener('mouseup', mouseUpHandler);
     }
 
     return () => {
       window.removeEventListener('mouseup', mouseUpHandler);
+      window.removeEventListener('mousemove', mouseMoveHandler);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dragging]);
 
   if (!containingPiece) return null;
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-    <div
-      className='movable-piece'
-      onMouseDown={() => setDragging(true)}
-      draggable={true}
-      hidden={!staticPieceVisible}
-    >
-      <Piece {...{ containingPiece }} />
-    </div>
+    <>
+      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+      <div
+        className='movable-piece-static'
+        onMouseDown={mouseDownHandler}
+        draggable={true}
+        style={{ display: staticPieceVisible ? null : 'none' }}
+      >
+        <Piece {...{ containingPiece }} />
+      </div>
+      <div
+        className='movable-piece-floating'
+        style={{
+          display: staticPieceVisible ? 'none' : null,
+          top: floatingPiecePosition.y,
+          left: floatingPiecePosition.x,
+        }}
+      >
+        <Piece {...{ containingPiece }} />
+      </div>
+    </>
   );
 }
