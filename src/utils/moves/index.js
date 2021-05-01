bd1663ba616a7e031ec1b2b921a22f702a3ed479
Brian Do <briancldo@gmail.com>
2021-05-01 10:20:11 -0400

    fixes check bug where king can move anywhere

--- src/utils/moves/index.js
+++ src/utils/moves/index.js
@@ -13,17 +13,17 @@ const piecesNeedExcludeLogic = new Set(['k', 'n', 'p']);
 export { default as makeMove } from './makeMoves';
 export function getPieceLegalMoves(board, square, piece) {
   if (!piece) piece = getPieceAtSquare(board, square);
-  const candidates = computeCandidateSquares[piece.type](
+  let candidates = computeCandidateSquares[piece.type](
     square,
     board,
     piece.color
   );
 
   if (!piecesNeedExcludeLogic.has(piece.type)) return candidates;
-  const squares = excludeOccupiedSquares(candidates, board, piece.color);
+  candidates = excludeOccupiedSquares(candidates, board, piece.color);
   if (getCheckedSide(board))
     return excludeNonCheckHandlingSquares(candidates, board);
-  return squares;
+  return candidates;
 }
 
 const computeCandidateSquares = {
