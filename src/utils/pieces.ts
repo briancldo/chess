0f2f38e6dc8563608a5dc10b2df41e6e25722768
Brian Do <briancldo@gmail.com>
2021-05-15 22:25:53 -0400

    fixes ts errors in pieces util and removes code superseded by TS

--- src/utils/pieces.ts
+++ src/utils/pieces.ts
@@ -1,47 +1,7 @@
 import { DevError } from './errors';
+import { Piece, PieceColor, PieceType } from './pieces.types';
 
-const validPieceTypes = new Set(['k', 'q', 'r', 'b', 'n', 'p']);
-const validPieceColors = new Set(['b', 'w']);
-
-function validateColor(color) {
-  if (!color) throw new DevError('Containing piece must have a color');
-  if (!validPieceColors.has(color))
-    throw new DevError(`Invalid piece color: ${color}`);
-}
-
-function validateType(type) {
-  if (!type) throw new DevError('Containing piece must have type.');
-  if (!validPieceTypes.has(type))
-    throw new DevError(`Invalid piece type: ${type}`);
-}
-
-export function validatePiece(piece) {
-  if (!piece) return;
-
-  if (typeof piece !== 'object' || !piece.color || !piece.type)
-    throw new DevError('Piece must have color and type properties.');
-
-  validateColor(piece.color);
-  validateType(piece.type);
-}
-
-function validatePieceString(pieceString) {
-  if (typeof pieceString !== 'string' || pieceString.length !== 2)
-    throw new DevError(`Piece string must be length 2 string: ${pieceString}`);
-
-  const [color, type] = pieceString.split('');
-  validateColor(color);
-  validateType(type);
-}
-
-export function constructPiece(pieceString) {
-  validatePieceString(pieceString);
-
-  const [color, type] = pieceString.split('');
-  return { color, type };
-}
-
-export const PIECES = {
+export const PIECES: { [side in PieceColor]: { [piece in PieceType]: Piece } } = {
   b: {
     k: { type: 'k', color: 'b' },
     q: { type: 'q', color: 'b' },
@@ -60,7 +20,7 @@ export const PIECES = {
   },
 };
 
-export function flipColor(color) {
+export function flipColor(color: PieceColor) {
   if (color === 'w') return 'b';
   if (color === 'b') return 'w';
   throw new DevError(`No such color: ${color}`);
