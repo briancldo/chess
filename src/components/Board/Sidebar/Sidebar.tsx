30f3398fcd38cc3179b569a8a8db2d67c4e9ff61
Brian Do <briancldo@gmail.com>
2021-05-23 18:07:46 -0400

    feat: adds a game over mode for the board side panel

--- src/components/Board/Sidebar/Sidebar.tsx
+++ src/components/Board/Sidebar/Sidebar.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import GameOver from './content/GameOver';
+import config from '../../../config/config';
 import './Sidebar.css';
 
+const colors = config.get('square.colors')['default'];
+const sidebarStyle = {
+  backgroundColor: colors.lightComplement,
+};
+
 const Sidebar: React.FC = () => {
-  return <div className='board-sidebar'></div>;
+  return (
+    <div className='board-sidebar' style={sidebarStyle}>
+      <GameOver />
+    </div>
+  );
 };
 
 export default Sidebar;
---
+++ src/components/Board/Sidebar/content/GameOver.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './GameOver.css';
+
+const SidebarGameOver: React.FC = () => {
+  return (
+    <div className='board-sidebar-game-over'>
+      <p>asd</p>
+    </div>
+  );
+};
+
+export default SidebarGameOver;
