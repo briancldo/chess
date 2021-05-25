7a7c6dd2631241c55c5fbd48a0c842bedad7fe07
Brian Do <briancldo@gmail.com>
2021-05-24 20:26:10 -0400

    Revert "refactor: removes temporary game over handler"
    This reverts commit 9f41e7e419fb28089238b275895d2cb2145bc5f7.

---
+++ src/utils/game.ts
@@ -0,0 +1,38 @@
+import {
+  GameResult,
+  GameResultValue,
+  GameWon,
+  GameDrawn,
+  GameResultMethod,
+} from './board.types';
+
+function isGameWon(result: GameResult): result is GameWon {
+  return result.value === GameResultValue.won;
+}
+
+function isGameDrawn(result: GameResult): result is GameDrawn {
+  return result.value === GameResultValue.drawn;
+}
+
+let gameOverHandled = false;
+export function tempHandleGameOver(result: GameResult) {
+  if (gameOverHandled) return;
+
+  const _method = getMethodLonghand(result.method);
+  if (isGameDrawn(result)) {
+    alert(`Draw by ${_method}.`);
+  }
+  if (isGameWon(result)) {
+    const sideLong = result.side === 'w' ? 'White' : 'Black';
+    alert(`${sideLong} wins by ${_method}.`);
+  }
+  gameOverHandled = true;
+}
+
+const methodMapping = {
+  s: 'stalemate',
+  c: 'checkmate',
+};
+function getMethodLonghand(methodShorthand: GameResultMethod) {
+  return methodMapping[methodShorthand];
+}
