4067287ad080f5058a7d937be5a72c446cdf1cb4
Brian Do <briancldo@gmail.com>
2021-05-27 23:25:11 -0400

    test: updates tests due to createConcisePosition api change

--- src/components/__tests__/unit/utils/boardEditor.test.ts
+++ src/components/__tests__/unit/utils/boardEditor.test.ts
@@ -3,14 +3,14 @@ import '@testing-library/jest-dom';
 import data from './support/boardEditor.data';
 import {
   createConcisePosition,
-  PieceSquarePair,
+  PiecePlacements,
 } from '../../../../utils/boardEditor';
 
 describe('#boardEditor', () => {
   describe('createConcisePosition()', () => {
     test('correctly sets up position', () => {
       for (let i = 0; i < data.concisePositions.length; i++) {
-        const concisePosition = data.concisePositions[i] as PieceSquarePair[];
+        const concisePosition = data.concisePositions[i] as PiecePlacements;
         const position = createConcisePosition(concisePosition);
         expect(position).toEqual(data.concisePositionResults[i]);
       }
