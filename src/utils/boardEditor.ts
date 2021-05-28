2528783be6e8b2ac9cc0f66eb1f3c6754d94f928
Brian Do <briancldo@gmail.com>
2021-05-27 22:36:26 -0400

    refactor: changes createConcisePosition to use piece string and square string

--- src/utils/boardEditor.ts
+++ src/utils/boardEditor.ts
@@ -1,8 +1,10 @@
 import { produce } from 'immer';
+import { squareStringToObject } from './board';
 import { EMPTY_POSITION } from './board.constants';
 import initialBoard from './board.init';
-import { Board, BoardSquare } from './board.types';
-import { Piece } from './pieces.types';
+import { Board, SquareString } from './board.types';
+import { pieceStringToObject } from './pieces';
+import { PieceString } from './pieces.types';
 
 export function createBoard(board: Partial<Board>) {
   return produce(initialBoard, (draft) => {
@@ -11,14 +13,14 @@ export function createBoard(board: Partial<Board>) {
   });
 }
 
-interface PieceSquarePair {
-  piece: Piece;
-  square: BoardSquare;
-}
+type PieceSquarePair = [PieceString, SquareString];
 
 export function createConcisePosition(pieceSquarePairs: PieceSquarePair[]) {
   return produce(EMPTY_POSITION, (draft) => {
-    for (const { piece, square } of pieceSquarePairs) {
+    for (const [pieceString, squareString] of pieceSquarePairs) {
+      const piece = pieceStringToObject(pieceString);
+      const square = squareStringToObject(squareString);
+
       const { rank, file } = square;
       draft[rank][file] = piece;
     }
