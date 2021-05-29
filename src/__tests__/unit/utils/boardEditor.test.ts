c11989e16852fb47a93ad32fbe9ee26a2aa2124b
Brian Do <briancldo@gmail.com>
2021-05-28 20:35:11 -0400

    test: adds test for createBoard - position

--- src/__tests__/unit/utils/boardEditor.test.ts
+++ src/__tests__/unit/utils/boardEditor.test.ts
@@ -2,11 +2,27 @@ import '@testing-library/jest-dom';
 
 import data from './support/boardEditor.data';
 import {
+  createBoard,
   createConcisePosition,
   PiecePlacements,
 } from '../../../utils/board/boardEditor';
+import { BoardPosition } from '../../../utils/board/board.types';
 
 describe('#boardEditor', () => {
+  describe('createBoard()', () => {
+    test('sets position as given', () => {
+      const position = [undefined, {}, {}, {}, {}, {}, {}, {}, {}];
+      const board = createBoard({ position: position as BoardPosition });
+      expect(board.position).toEqual([...position]);
+    });
+
+    // test('sets state as given');
+
+    // test('sets default position if not given');
+
+    // test('sets default state if not given');
+  });
+
   describe('createConcisePosition()', () => {
     test('correctly sets up position', () => {
       for (let i = 0; i < data.concisePositions.length; i++) {
