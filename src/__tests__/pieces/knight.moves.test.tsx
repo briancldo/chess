646ff80615ce0d2c0766cfbade3e0982c03a2892
Brian Do <briancldo@gmail.com>
2021-06-02 17:54:20 -0400

    test: adds actual move test cases for knight

--- src/__tests__/pieces/knight.moves.test.tsx
+++ src/__tests__/pieces/knight.moves.test.tsx
@@ -1,22 +1,42 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import * as data from './support/knight.moves.data';
-import { assertCandidateMoves } from './common.test.utils';
+import { assertCandidateMoves, assertMadeMoves } from './common.test.utils';
 
 describe('#knight.moves', () => {
-  test('pure knight moves', () => {
-    assertCandidateMoves(data.happyPositionsAndMoves);
+  describe('candidate moves', () => {
+    test('pure knight moves', () => {
+      assertCandidateMoves(data.happyPositionsAndMoves);
+    });
+
+    test('cannot move to ally piece square, can move to enemy piece square', () => {
+      assertCandidateMoves(data.otherPieceOccupyPositionsAndMoves);
+    });
+
+    test('cannot move to squares off side of board', () => {
+      assertCandidateMoves(data.offsideMoves);
+    });
+
+    test('can hop over other pieces', () => {
+      assertCandidateMoves(data.hopOverOthersPositionAndMoves);
+    });
   });
 
-  test('cannot move to ally piece square, can move to enemy piece square', () => {
-    assertCandidateMoves(data.otherPieceOccupyPositionsAndMoves);
-  });
+  describe('actual moves', () => {
+    test('pure knight moves', () => {
+      assertMadeMoves(data.happyPositionsAndMoves);
+    });
 
-  test('cannot move to squares off side of board', () => {
-    assertCandidateMoves(data.offsideMoves);
-  });
+    test('cannot move to ally piece square, can move to enemy piece square', () => {
+      assertMadeMoves(data.otherPieceOccupyPositionsAndMoves);
+    });
 
-  test('can hop over other pieces', () => {
-    assertCandidateMoves(data.hopOverOthersPositionAndMoves);
+    test('cannot move to squares off side of board', () => {
+      assertMadeMoves(data.offsideMoves);
+    });
+
+    test('can hop over other pieces', () => {
+      assertMadeMoves(data.hopOverOthersPositionAndMoves);
+    });
   });
 });
