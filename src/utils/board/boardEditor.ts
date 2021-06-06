b5041942cc371700e99a3387b92a3b38fcb83642
Brian Do <briancldo@gmail.com>
2021-06-06 18:24:23 -0400

    feat: createBoard creates king square state based off of kings in position

--- src/utils/board/boardEditor.ts
+++ src/utils/board/boardEditor.ts
@@ -6,27 +6,72 @@ import { EMPTY_POSITION } from './board.constants';
 import initialBoard from './board.init';
 import {
   BoardFile,
+  BoardKingState,
   BoardPosition,
   BoardRank,
+  BoardSquare,
   BoardSubstate,
   Coordinate,
 } from './board.types';
 import { pieceObjectToString, pieceStringToObject } from '../pieces';
 import { PieceString } from '../pieces.types';
+import { DevError } from '../errors';
 
 export function createBoard(board: {
   position?: ConcisePosition;
   state?: BoardSubstate;
 }) {
   const { position, state } = board;
+  const syncedState = synchronizeState(position, state);
   return {
     position: position
       ? createFromConcisePosition(position)
       : initialBoard.position,
-    state: state
-      ? assign(cloneDeep(initialBoard.state), state)
-      : initialBoard.state,
+    state: syncedState,
   };
+}
+
+function synchronizeState(position?: ConcisePosition, state?: BoardSubstate) {
+  const kingState = synchronizeKingState(position, state);
+  const syncedState = state
+    ? assign({}, initialBoard.state, state)
+    : initialBoard.state;
+  return assign({}, syncedState, { king: kingState });
+}
+
+function synchronizeKingState(
+  position?: ConcisePosition,
+  state?: BoardSubstate
+): BoardKingState {
+  const priorKingState: BoardKingState = produce(
+    initialBoard.state.king,
+    (draft) => {
+      if (state?.king?.w?.square)
+        draft.w.square = state.king.w.square as BoardSquare;
+      if (state?.king?.b?.square)
+        draft.b.square = state.king.b.square as BoardSquare;
+    }
+  );
+
+  return produce(priorKingState, (draft) => {
+    if (!position) return;
+    if (Array.isArray(position.wk) && position.wk.length !== 1)
+      throw new DevError('Only one white king.');
+    if (Array.isArray(position.bk) && position.bk.length !== 1)
+      throw new DevError('Only one black king.');
+
+    const whiteKingCoordinate = Array.isArray(position.wk)
+      ? position.wk.pop()
+      : position.wk;
+    const blackKingCoordinate = Array.isArray(position.bk)
+      ? position.bk.pop()
+      : position.bk;
+
+    if (position.wk)
+      draft.w.square = coordinateToSquare(whiteKingCoordinate as Coordinate);
+    if (position.bk)
+      draft.b.square = coordinateToSquare(blackKingCoordinate as Coordinate);
+  });
 }
 
 export type ConcisePosition = {
