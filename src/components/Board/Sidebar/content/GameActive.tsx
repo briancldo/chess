bc27b56a884ee967a8ef02d98fc31b32dadabca6
Brian Do <briancldo@gmail.com>
2021-06-17 11:10:38 -0400

    feat: gameview displays the captured pieces

--- src/components/Board/Sidebar/content/GameActive.tsx
+++ src/components/Board/Sidebar/content/GameActive.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
 import { GameActiveSidebarProps } from '../Sidebar.types';
+import Piece from '../../../Pieces/Piece';
 import './GameActive.css';
 
 const GameActive: React.FC<GameActiveSidebarProps> = (props) => {
   const { board } = props;
+  const { capturedPieces } = board.state;
+
   return (
     <div className='board-sidebar-game-active'>
-      <p>Board: {JSON.stringify(board.state.capturedPieces, null, 2)}</p>
+      <div className='sidebar-game-active-captured-piece-row'>
+        {capturedPieces.b.map((pieceType) => (
+          <div
+            key={`captured-b-${pieceType}`}
+            className='sidebar-game-active-captured-piece'
+          >
+            <Piece containingPiece={{ color: 'b', type: pieceType }} />
+          </div>
+        ))}
+      </div>
+      <div className='sidebar-game-active-body' />
+      <div className='sidebar-game-active-captured-piece-row'>
+        {capturedPieces.w.map((pieceType) => (
+          <div
+            key={`captured-w-${pieceType}`}
+            className='sidebar-game-active-captured-piece'
+          >
+            <Piece containingPiece={{ color: 'w', type: pieceType }} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
