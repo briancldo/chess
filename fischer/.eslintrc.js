5ebad0a9ceee8f53a8d3ed3331c7579f0f371c1c
Brian Do <briancldo@gmail.com>
2021-07-03 11:10:50 -0400

    chore(fischer): updates tooling to be more monorepo-friendly

---
+++ fischer/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest-formatting/recommended',
+    'plugin:node/recommended',
+    'plugin:promise/recommended',
+    'plugin:security/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    //   project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'prettier',
+    'sonarjs',
+    'json-format',
+    'promise',
+    'jest',
+    'jest-formatting',
+    'security',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  settings: {
+    jest: {
+      version: 'detect',
+    },
+  },
+};
