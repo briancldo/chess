453cc6d4b0323e0fe020df9f505d2af241ca5562
Brian Do <briancldo@gmail.com>
2021-07-08 14:18:28 -0400

    feat: adds login button to app

--- morphy/src/App.tsx
+++ morphy/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import LoginOrOutButton from './components/Login/LoginOrOutButton';
 import Router from './pages/Router';
+import './App.css';
 
 export default function App() {
-  return <Router />;
+  return (
+    <>
+      <div className='login-or-out-button'>
+        <LoginOrOutButton />
+      </div>
+      <Router />
+    </>
+  );
 }
