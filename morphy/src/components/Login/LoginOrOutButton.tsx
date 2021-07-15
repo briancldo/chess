243e7926e8a21752ea68c22571a7dd85e474a201
Brian Do <briancldo@gmail.com>
2021-07-15 15:45:41 -0400

    test(morphy): restores socket connection on reload if logged in

--- morphy/src/components/Login/LoginOrOutButton.tsx
+++ morphy/src/components/Login/LoginOrOutButton.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import useUserStore from '../../store/user';
+import { useReconnect } from '../../utils/user/login/reconnect';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 
 const LoginOrOutButton: React.FC = () => {
+  useReconnect();
   const isLoggedIn = useUserStore((state) => state.isLoggedIn);
   const AuthButton = isLoggedIn ? LogoutButton : LoginButton;
 
