2bb5ffe8f4fef49b3b5004e2880bd70b204c5e18
Brian Do <briancldo@gmail.com>
2021-08-13 22:48:01 -0400

    chore(carlsen): installs and configures playwright

---
+++ carlsen/playwright.config.ts
@@ -0,0 +1,45 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  use: {
+    baseURL: 'http://localhost:3000',
+  },
+  projects: [
+    {
+      name: 'Chrome Stable',
+      use: {
+        browserName: 'chromium',
+        channel: 'chrome',
+      },
+    },
+    {
+      name: 'Desktop Firefox',
+      use: {
+        browserName: 'firefox',
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: 'Desktop Safari',
+      use: {
+        browserName: 'webkit',
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        browserName: 'chromium',
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        browserName: 'webkit',
+        ...devices['iPhone 12'],
+      },
+    },
+  ],
+};
+export default config;
