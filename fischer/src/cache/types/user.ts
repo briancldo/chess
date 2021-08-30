b5e97175208158a3c7669d5dec05ded119abd397
Brian Do <briancldo@gmail.com>
2021-08-29 23:40:53 -0400

    feat(fischer): user cache can be indexed by username or user id

--- fischer/src/cache/types/user.ts
+++ fischer/src/cache/types/user.ts
@@ -1,3 +1,24 @@
+import NodeCache from 'node-cache';
+
+type UserId = string;
+type Username = string;
 export interface UserInfo {
-  username: string;
+  id: UserId;
+  username: Username;
+}
+
+// @ts-expect-error: want to override in my own way
+export interface UserCacheById extends NodeCache {
+  set(id: UserId, userInfo: UserInfo): boolean;
+  get(id: UserId): UserInfo | undefined;
+  has(id: UserId): boolean;
+  del(id: UserId): number;
+}
+
+// @ts-expect-error: want to override in my own way
+export interface UserCacheByUsername extends NodeCache {
+  set(username: Username, id: UserId): boolean;
+  get(username: Username): UserId | undefined;
+  has(username: Username): boolean;
+  del(username: Username): number;
 }
