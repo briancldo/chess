4d76360447bba694bf20630ac5c0bad5c9529cc7
Brian Do <briancldo@gmail.com>
2021-09-21 17:01:41 -0400

    test(fischer): sides are randomly assigned to the two players

--- fischer/src/cache/match/types.ts
+++ fischer/src/cache/match/types.ts
@@ -1,5 +1,15 @@
 import { UserId } from '../user/types';
 
+export interface MatchGameSideDetails {
+  white: UserId;
+  black: UserId;
+}
+
+interface MatchGameDetails {
+  sides: MatchGameSideDetails;
+}
+
 export interface MatchDetails {
   players: [UserId, UserId];
+  gameDetails: MatchGameDetails;
 }
