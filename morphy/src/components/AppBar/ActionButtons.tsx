c207fdbc38ce1dfc7dd25f7b16a54ee2185c4180
briancldo <35510068+briancldo@users.noreply.github.com>
2021-11-13 23:11:46 -0500

    feat(morphy): removes ping button

--- morphy/src/components/AppBar/ActionButtons.tsx
+++ morphy/src/components/AppBar/ActionButtons.tsx
@@ -4,15 +4,18 @@ import useUserStore from '../../store/user';
 import LoginSection from '../Login/LoginSection';
 import ChallengeButton from '../misc/ChallengeButton';
 import PingButton from '../misc/PingButton';
+import config from '../../config/config';
 
 import './AppActionButtons.css';
+
+const { environment } = config;
 
 const ActionButtons: React.FC = () => {
   const { isLoggedIn } = useUserStore();
 
   return (
     <div className='app-action-buttons'>
-      <PingButton />
+      {environment !== 'production' && <PingButton />}
       {isLoggedIn && <ChallengeButton />}
       <LoginSection />
     </div>
